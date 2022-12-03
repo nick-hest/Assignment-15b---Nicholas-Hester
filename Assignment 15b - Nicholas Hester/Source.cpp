bd88637d64cdb6bdf1d1ebc5f34d169b4ac19c57
NickHest <nick@hest.com>
2022-12-03 15:22:27 -0500

    Finished both half functions' code.

--- Assignment 15b - Nicholas Hester/Source.cpp
+++ Assignment 15b - Nicholas Hester/Source.cpp
@@ -7,13 +7,16 @@ using namespace std;
 
 int half(int input)
 {
-
+	if ((input / 2.0) > .5)
+		return input / 2 + 1;
+	else
+		return input / 2;
 }
 
 template <typename A>
 A half(A input)
 {
-
+	return input / 2;
 }
 
 int main()
