98b3b85694a13335927d8b8538983de93082ce25
NickHest <nick@hest.com>
2022-12-03 15:20:37 -0500

    Performed general setup, added prototypes for the two half functions, and added a basic main function.

---
+++ Assignment 15b - Nicholas Hester/Source.cpp
@@ -0,0 +1,22 @@
+// Nicholas Hester
+// CIS 1202-501
+// December 3, 2022
+
+#include <iostream>
+using namespace std;
+
+int half(int input)
+{
+
+}
+
+template <typename A>
+A half(A input)
+{
+
+}
+
+int main()
+{
+	return 0;
+}
