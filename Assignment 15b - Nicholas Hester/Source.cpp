8f335a9d048e27f7fc7d03f86033b70dbe3d494b
NickHest <nick@hest.com>
2022-12-03 15:24:12 -0500

    Finished the main function's test code.

--- Assignment 15b - Nicholas Hester/Source.cpp
+++ Assignment 15b - Nicholas Hester/Source.cpp
@@ -21,5 +21,13 @@ A half(A input)
 
 int main()
 {
+	double a = 7.0;
+	float b = 5.0f;
+	int c = 3;
+
+	cout << half(a) << endl;
+	cout << half(b) << endl;
+	cout << half(c) << endl;
+
 	return 0;
 }
